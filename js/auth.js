bf5add70b5020443bb16bfe7b09ed26fea195ccf
Senpauwka <168306935+Senpauwka@users.noreply.github.com>
2026-08-05 23:23:47 +0300

    Update auth.js

---
+++ js/auth.js
@@ -0,0 +1,33 @@
+const demoUser = {
+    name: "Гость",
+    email: "",
+    loggedIn: false
+};
+
+if (!localStorage.getItem("user")) {
+    localStorage.setItem("user", JSON.stringify(demoUser));
+}
+
+function getUser() {
+    return JSON.parse(localStorage.getItem("user"));
+}
+
+function login(name, email) {
+
+    const user = {
+        name,
+        email,
+        loggedIn: true
+    };
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    location.href = "profile.html";
+}
+
+function logout() {
+
+    localStorage.setItem("user", JSON.stringify(demoUser));
+
+    location.href = "index.html";
+}
